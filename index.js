81208c7b84b259229eb7230848f4889f7fcecfe7
Kevin Lai <kevinlai247@gmail.com>
2021-09-14 15:25:14 -0700

    Add change title logic

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+
+    let titlesList = ["app","blog","shop","note","game","grave"];
+    let titleIdx = 0;
+
+    let currentTitle = document.getElementById("title-current");
+
+    currentTitle.innerHTML = titlesList[titleIdx];
+
+    let leftButton = document.getElementById("left-button");
+    let rightButton = document.getElementById("right-button");
+
+    leftButton.addEventListener("click", ()=>{
+        if(titleIdx === 0){
+            titleIdx = titlesList.length-1;
+        }else{
+            titleIdx--;
+        }
+        currentTitle.innerHTML = titlesList[titleIdx];
+    });
+
+    rightButton.addEventListener("click", ()=>{
+        if(titleIdx === titlesList.length-1){
+            titleIdx = 0;
+        }else{
+            titleIdx++;
+        }
+        currentTitle.innerHTML = titlesList[titleIdx];
+    });
+
 });
